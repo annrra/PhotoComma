75aa64e586b090f9ea8a2162c002741f1b158814
annrra <annrra@gmail.com>
2026-04-30 14:32:37 +0300

    Undo added Content Security Policy headers

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,5 @@
 import type { NextConfig } from "next";
 
-const securityHeaders = [
-  {
-    key: "Content-Security-Policy",
-    value: `
-      default-src 'self';
-      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://va.vercel-scripts.com;
-      style-src 'self' 'unsafe-inline';
-      img-src 'self' data: blob: https:;
-      font-src 'self' data: https:;
-      connect-src 'self' https://api.photocomma.com https://va.vercel-scripts.com https://vitals.vercel-insights.com;
-      media-src 'self' https://api.photocomma.com;
-      frame-ancestors 'none';
-    `.replace(/\n/g, ""),
-  },
-  {
-    key: "X-Frame-Options",
-    value: "DENY",
-  },
-  {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
-  },
-  {
-    key: "Referrer-Policy",
-    value: "strict-origin-when-cross-origin",
-  },
-  {
-    key: "Strict-Transport-Security",
-    value: "max-age=63072000; includeSubDomains; preload",
-  },
-];
-
 const nextConfig: NextConfig = {
   images: {
 		remotePatterns: [
@@ -54,15 +22,6 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
-  },
-
-  async headers() {
-    return [
-      {
-        source: "/(.*)", // applies to ALL routes
-        headers: securityHeaders,
-      },
-    ];
   },
 };
 
