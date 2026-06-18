2ce3734fccf9ee588614bf35ce7f3faa92a55535
annrra <annrra@gmail.com>
2026-06-18 15:38:22 +0300

    Update prints info panel content and refine CSS styles

--- src/components/PrintsView/PrintsInfoPanel.tsx
+++ src/components/PrintsView/PrintsInfoPanel.tsx
@@ -1,18 +1,20 @@
 import { SeparatorDecorator } from '@/src/components/ui/SeparatorDecorator';
 import styles from './pv.module.css';
 import { EmailLink } from '@/src/components/_utils/EmailLink';
+import Link from 'next/link';
 
 const PrintsInfoPanel = () => {
 
   return (
     <div className={styles.segment}>
       <SeparatorDecorator />
-      <h1 className={styles.sronly}>Prints and shipping information at PhotoComma</h1>
-      Each photograph is available to be purchased as a print<br />
-      Printed with care<br />
-      Shipping worldwide<br /><br />
+      <h1 className={styles.sronly}>Photography Prints and Small Publications Store | PhotoComma</h1>
+      All photographs are available as unframed fine art prints.<br />
+      Printed and packed with care.<br />
+      Available in a selection of sizes.<br />
+      Shipping worldwide.<br /><br />
 
-      For print requests: <EmailLink showEmail className={styles.mailto} />
+      Other prints available on request: <EmailLink showEmail className={styles.mailto} />
     </div>
   )
 }
